0743166c53ff17947541a21d995dab3aaa14b267
Gurumurthy, Karthik <a514035@fmr.com>
2022-08-02 15:28:42 -0400

    odyssey liftoff part 2 complete

---
+++ server/src/index.js
@@ -0,0 +1,28 @@
+const { ApolloServer } = require('apollo-server')
+const {
+  ApolloServerPluginLandingPageLocalDefault,
+} = require('apollo-server-core')
+const { bootstrap: bootstrapGlobalAgent } = require('global-agent')
+const typeDefs = require('./schema')
+const resolvers = require('./resolvers')
+const TrackAPI = require('./datasources/track-api')
+
+bootstrapGlobalAgent()
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => {
+    return {
+      trackAPI: new TrackAPI(),
+    }
+  },
+})
+
+server.listen().then(() => {
+  console.log(`
+    🚀  Server is running!
+    🔉  Listening on port 4000
+    📭  Query at http://localhost:4000
+`)
+})
